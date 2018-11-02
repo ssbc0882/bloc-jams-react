fe4d068a071608262c9259d8c18651c1e169b3cb
Br <br@Brs-MacBook-Pro.local>
2018-11-02 16:07:05 -0700

    added information to the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,7 +18,6 @@ const Landing = () => (
                 <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
             </div>
         </section>
-
     </section>
 );
 
